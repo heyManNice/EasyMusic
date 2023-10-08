dfad80fdcedf68fd3b263cf77459509cb2cc99f5
NiceFish4520 <kcass774@gmail.com>
2023-10-08 23:12:02 +0800

    feat: apiservice

--- apiservice.cpp
+++ apiservice.cpp
@@ -1,18 +1,26 @@
 #include "apiservice.h"
 
-//#include "apiservice.h"
+#include <cpr/cpr.h>
 
-void apiservice::from_json(const nlohmann::json& j, Artist& artist_) {
+constexpr auto APIDOMAIN = "http://cloud-music.pl-fe.cn";
+
+template<typename InputType>
+std::tuple<int, std::string> apiservice::HTTP_Get(InputType url) {
+	auto rep = cpr::Get(cpr::Url{ url });
+	return std::make_tuple((int)rep.error.code, rep.text);
+}
+
+void apiservice::impl::from_json(const nlohmann::json& j, Artist& artist_) {
 	artist_.id = j["id"].get<uint32_t>();
 	artist_.name = j["name"].get<std::string>();
 }
 
-void apiservice::from_json(const nlohmann::json& j, Album& album_) {
+void apiservice::impl::from_json(const nlohmann::json& j, Album& album_) {
 	album_.id = j["id"].get<uint32_t>();
 	album_.name = j["name"].get<std::string>();
 }
 
-void apiservice::from_json(const nlohmann::json& j, Song& song_) {
+void apiservice::impl::from_json(const nlohmann::json& j, Song& song_) {
 	song_.name = j["name"].get<std::string>();
 	song_.id = j["id"].get<uint32_t>();
 	song_.artists = j["artists"].get<Artists>();
@@ -20,7 +28,7 @@ void apiservice::from_json(const nlohmann::json& j, Song& song_) {
 	song_.duration = j["duration"].get<uint32_t>();
 }
 
-void apiservice::from_json(const nlohmann::json& j, SignalSong& song_) {
+void apiservice::impl::from_json(const nlohmann::json& j, SignalSong& song_) {
 	Song song__{};
 	song__.name = j["name"].get<std::string>();
 	song__.id = j["id"].get<uint32_t>();
@@ -30,15 +38,35 @@ void apiservice::from_json(const nlohmann::json& j, SignalSong& song_) {
 	song_.albumPicUrl = j["al"]["picUrl"].get<std::string>();
 }
 
-void apiservice::from_json(const nlohmann::json& j, SongResult& sonyresult_) {
+void apiservice::impl::from_json(const nlohmann::json& j, SongResult& sonyresult_) {
 	sonyresult_.song = j["songs"][0].get<SignalSong>();
 	sonyresult_.code = j["code"].get<int>();
 }
 
-void apiservice::from_json(const nlohmann::json& j, SearchResult& searchresult_) {
+void apiservice::impl::from_json(const nlohmann::json& j, SearchResult& searchresult_) {
 	searchresult_.code = j["code"].get<int>();
 	auto result = j["result"];
 	searchresult_.songs = result["songs"].get<Songs>();
 	searchresult_.hasMore = result["hasMore"].get<bool>();
 	searchresult_.songCount = result["songCount"].get<uint32_t>();
+}
+
+std::tuple<int, apiservice::SongResultPtr> apiservice::GetSong(int id)
+{
+	std::string url = std::format("{}/song/detail?ids={}", APIDOMAIN,id);
+	auto [code, data] = HTTP_Get(url);
+	if(code)
+		return std::make_tuple<int, SongResultPtr>((int)code, nullptr);
+	auto songresult = apiservice::impl::parse<apiservice::impl::SongResult>(data);
+	return std::make_tuple<int, SongResultPtr>(0, std::make_shared<apiservice::impl::SongResult>(songresult));
+}
+
+std::tuple<int, apiservice::SearchResultPtr> apiservice::SearchSong(std::string_view keyword, int limit)
+{
+	std::string url = std::format("{}/search?limit={}&keywords={}", APIDOMAIN, limit, keyword);
+	auto [code, data] = HTTP_Get(url);
+	if (code)
+		return std::make_tuple<int, SearchResultPtr>((int)code, nullptr);
+	auto songresult = apiservice::impl::parse<apiservice::impl::SearchResult>(data);
+	return std::make_tuple<int, SearchResultPtr>(0, std::make_shared<apiservice::impl::SearchResult>(songresult));
 }
--- apiservice.h
+++ apiservice.h
@@ -4,63 +4,75 @@
 #include <vector>
 #include <string>
 #include <string_view>
+#include <memory>
 
 #include <nlohmann/json.hpp>
 
 namespace apiservice {
-	struct Artist
-	{
-		uint32_t id;
-		std::string name;
-	};
-	typedef std::vector<Artist> Artists;
-	void from_json(const nlohmann::json& j, Artist& artist_);
+	namespace impl {
+		struct Artist
+		{
+			uint32_t id;
+			std::string name;
+		};
+		typedef std::vector<Artist> Artists;
+		void from_json(const nlohmann::json& j, Artist& artist_);
 
-	struct Album
-	{
-		uint32_t id;
-		std::string name;
-	};
-	void from_json(const nlohmann::json& j, Album& album_);
+		struct Album
+		{
+			uint32_t id;
+			std::string name;
+		};
+		void from_json(const nlohmann::json& j, Album& album_);
 
-	struct Song
-	{
-		std::string name;
-		uint32_t id;
-		Artists artists;
-		Album album;
-		uint32_t duration;
-	};
-	typedef std::vector<Song> Songs;
-	void from_json(const nlohmann::json& j, Song& song_);
+		struct Song
+		{
+			std::string name;
+			uint32_t id;
+			Artists artists;
+			Album album;
+			uint32_t duration;
+		};
+		typedef std::vector<Song> Songs;
+		void from_json(const nlohmann::json& j, Song& song_);
 
-	struct SignalSong
-	{
-		Song song;
-		std::string albumPicUrl;
-	};
-	struct SongResult
-	{
-		SignalSong song;
-		int code;
-	};
-	void from_json(const nlohmann::json& j, SignalSong& song_);
-	void from_json(const nlohmann::json& j, SongResult& sonyresult_);
+		struct SignalSong
+		{
+			Song song;
+			std::string albumPicUrl;
+		};
+		struct SongResult
+		{
+			SignalSong song;
+			int code;
+		};
+		void from_json(const nlohmann::json& j, SignalSong& song_);
+		void from_json(const nlohmann::json& j, SongResult& sonyresult_);
 
-	struct SearchResult
-	{
-		Songs songs;
-		bool hasMore;
-		uint32_t songCount;
-		int code;
+		struct SearchResult
+		{
+			Songs songs;
+			bool hasMore;
+			uint32_t songCount;
+			int code;
+		};
+
+		void from_json(const nlohmann::json& j, SearchResult& searchresult_);
+
+		template<typename T, typename Input>
+		T parse(Input sv) {
+			return (T)nlohmann::json::parse(sv);
+		}
 	};
 
-	void from_json(const nlohmann::json& j, SearchResult& searchresult_);
+	template<typename InputType>
+	std::tuple<int, std::string> HTTP_Get(InputType url);
 
-	template<typename T, typename Input>
-	T parse(Input sv) {
-		return (T)nlohmann::json::parse(sv);
-	}
+	typedef std::shared_ptr<impl::SongResult> SongResultPtr;
+	std::tuple<int, SongResultPtr> GetSong(int id);
+
+	typedef std::shared_ptr<impl::SearchResult> SearchResultPtr;
+	std::tuple<int, SearchResultPtr> SearchSong(std::string_view keyword, int limit = 12);
 
 }
 
